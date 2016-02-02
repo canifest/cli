e07feee7a90e500c9b79442ee33850a33ec78507
Michael Rice <mrice@redhat.com>
2016-02-02 15:48:27 -0700

    initial cut of a hello, world cli

---
+++ cli.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/abiosoft/ishell"
+)
+
+func main() {
+	shell := ishell.New()
+	shell.Println("--[ canifest ]-----")
+	shell.SetPrompt(">")
+
+	shell.Register("hello", func(args ...string) (string, error) {
+		shell.ShowPrompt(false)
+		defer shell.ShowPrompt(true)  // interesting for reverting state
+		
+		shell.Print("What is your name? ")
+		name := shell.ReadLine()
+
+		return "Hello " + name, nil
+	})
+
+	shell.Start()
+}
